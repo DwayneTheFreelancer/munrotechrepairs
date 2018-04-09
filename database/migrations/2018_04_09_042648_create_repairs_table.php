d5b614cf731e89a66d321f66633e5234cbe9b677
Dwayne Munro <dwaynemunro@Dwaynes-MBP.fios-router.home>
2018-04-09 00:33:53 -0400

    readded repairs table to database

---
+++ database/migrations/2018_04_09_042648_create_repairs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepairsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repairs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('iphone');
+            $table->string('problem');
+            $table->text('first_name');
+            $table->text('last_name');
+            $table->text('email');
+            $table->text('number');
+            $table->text('address');
+            $table->text('zip_code');
+            $table->text('note');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('repairs');
+    }
+}
