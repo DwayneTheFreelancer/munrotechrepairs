b69a2f58b72f968f778f7e2ea61ac35a95b19967
Dwayne Munro <dwaynemunro@Dwaynes-MBP.fios-router.home>
2018-04-24 08:39:30 -0400

    made second link center

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -19,7 +19,7 @@
 
     <div class="coming-soon">
         <h1 class="text-center"><a href="https://www.facebook.com/munrotechrepiars/">Like and Follow Us On FaceBook</a></h1>
-        <span class="text-center"><a href="https://www.facebook.com/munrotechrepiars/"><i class="fab fa-facebook-square fa-3x"></i></a></span>
+        <span><a class="text-center" href="https://www.facebook.com/munrotechrepiars/"><i class="fab fa-facebook-square fa-3x"></i></a></span>
     </div>
 
     <h1 class="text-center">How It Works</h1>
